977d5cce2d1592bec650599f07095f3dd8c900ab
Raiju 20 <ivanberestovoy9@gmail.com>
2024-09-25 08:25:34 +0000

    best

--- src/lib/TopMenu.svelte
+++ src/lib/TopMenu.svelte
@@ -1,36 +1,45 @@
 <script>
-    export let selectcomp
+    let selectedIndex = 0;
+    export let selectcomp;
+    let menuItems = [
+        {label: "Menu item 1" },
+        {label: "Menu item 2" },
+        {label: "Menu item 3" },
+        {label: "Menu item 4" },
+        // Add more items here...
+    ];
 </script>
 
-<div class = "topmenu">
-    <div on:click={()=>selectcomp(0)}>
-        Menu item 1
-    </div>
-
-    <div on:click={()=>selectcomp(1)}>
-        Menu item 2
-    </div>
-
-    <div on:click={()=>selectcomp(2)}>
-        Menu item 3
-    </div>
-
-    <div on:click={()=>selectcomp(3)}>
-        Menu item 4
-    </div>
+<div class="topmenu">
+    {#each menuItems as item, i}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div
+            class:selected={selectedIndex === i}
+            on:click={() => {
+                selectcomp(i);
+                selectedIndex = i;
+            }}
+        >
+            {item.label}
+        </div>
+    {/each}
 </div>
 
 <style>
     .topmenu {
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: space-between;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: space-between;
     }
 
     .topmenu div {
-    cursor: pointer;
-
+        cursor: pointer;
     }
 
+    .selected {
+        background-color: #ccc; /* add your desired styles here */
+        color: #333;
+    }
 </style>
